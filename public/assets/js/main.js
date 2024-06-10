38f3897c07b190caec82b6daba5b6c5122233779
kezi9 <kmeliana@myseneca.ca>
2024-06-10 18:09:25 -0400

    Updated organised html and js
    Organised sections (heading, content, interaction) both in html and js.
    Fetch link for other date in js
    Commented interaction part

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,19 +1,67 @@
+// require('dotenv').config();
+// let apikey = process.env.APIKEY
+// console.log(apikey);
+
 async function getData(apikey) {
     try {
+        // -- NASA API
+        // Astronomy Picture of the Day
         const result = await fetch(
             `https://api.nasa.gov/planetary/apod?api_key=${apikey}`
         );
-        const epicInfoServer = await result.json()
+        // 
+        const prevDate = await fetch(
+            `https://api.nasa.gov/planetary/apod?date=2024-06-09&api_key=${apikey}`
+        );
+        const APODServer = await result.json()
+        const dateServer = await prevDate.json()
 
-        // console.log(epicInfoServer);
+        // console.log(dateServer);
+        // console.log(APODServer);
+
+        // HTML calls
         const content = document.querySelector("#APOD");
-        content.innerHTML = `
-        <h1>${epicInfoServer.title}</h1>
-        <img src="${epicInfoServer.url}" alt="${epicInfoServer.media_type}"/>
-        <p>
-            ${epicInfoServer.explanation}
-        </p>
+        const apodTitle = document.querySelector("#APOD-title");
+        const apodDate = document.querySelector("#APOD-date");
+        const apodContent = document.querySelector("#APOD-content");
+        const apodInputDate = document.querySelector("#APOD-inputDate");
+
+        // HTML ADD
+        // -- Heading
+        apodTitle.innerHTML = `${APODServer.title}`;
+        apodDate.innerHTML = `${APODServer.date}`;
+
+        // -- Content
+        apodContent.innerHTML = `
+            <img src="${APODServer.url}" alt="${APODServer.media_type}"/>
+            <p>
+                ${APODServer.explanation}
+            </p>
         `;
+
+        // -- Interaction
+        // if {
+        //     const apodInputDate = document.querySelector("#APOD-inputDate").value = "2024-06-09"
+
+        //     apodInputDate.innerHTML = `
+        //         ${dateServer.title}
+        //         ${dateServer.url}
+        //         ${dateServer.date}
+        //         ${dateServer.explanation}
+        //     `
+        // } else {
+        //     ''
+        // };
+
+
+        // example
+        // content.innerHTML = `
+        // <h1>${APODServer.title}</h1>
+        // <img src="${APODServer.url}" alt="${APODServer.media_type}"/>
+        // <p>
+        //     ${APODServer.explanation}
+        // </p>
+        // `;
     } catch (error) {
         console.log(`Error: `, error);
     }
